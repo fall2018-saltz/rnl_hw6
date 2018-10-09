4319cd24b4f3b7850defb11b72c4604d26187355
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:17:33 -0400

    removing genBoxplots.R

--- genBoxplots.R
+++
@@ -1,5 +0,0 @@
-
-library(ggplot2)
-histPop <- ggplot(merged_data, aes(x=population))
-histPop <- histPop + geom_histogram(binwidth=5000000, color="black", fill="white")
-histPop <- histPop + ggtitle("State Population Histogram")
