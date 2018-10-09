645b616754b15a912c1189c6321a3f53dea91813
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:20:51 -0400

    Saving genBoxplots.R

--- genBoxplots.R
+++ genBoxplots.R
@@ -1,1 +1,7 @@
 
+# Create a boxplot for population
+boxPop <- ggplot(merged_data, aes(x=factor(0),population)) + geom_boxplot() + ggtitle('State Population Boxplot')
+
+# Create a boxplot for murder rate
+boxMurder <- ggplot(merged_data, aes(x=factor(0),Murder)) + geom_boxplot() + ggtitle('State Murder Rate Boxplot')
+
