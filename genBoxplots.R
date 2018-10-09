78bc0eb612e40c1854bbd682444fab9ca9af8c26
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:14:56 -0400

    Saving genBoxplots.R

--- genBoxplots.R
+++ genBoxplots.R
@@ -1,1 +1,4 @@
 
+histPop <- ggplot(merged_data, aes(x=Population))
+histPop <- histPop + geom_histogram(binwidth=5000000, color="black", fill="white")
+histPop <- histPop + ggtitle("State Population Histogram")
