484223877bcb3f903499629937e404b083dbf0a1
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-09 15:27:02 -0400

    Saving genBars.R

--- genBars.R
+++ genBars.R
@@ -3,13 +3,14 @@
 merged_data$TotalMurders <- (merged_data$population*erged_data$Murder)/100000
 
 # Generate a bar chart with the number of murders per state
-barMurder <- ggplot(arrestsPop, aes(x=State, y=TotalMurders, group = 1)) + geom_col() 
+library(ggplot2)
+barMurder <- ggplot(merged_data, aes(x=State, y=TotalMurders, group = 1)) + geom_col() 
 
 # Using the above bar chart, rotate the text on the x axis
-barMurder <- ggplot(arrestsPop, aes(x=State, y=TotalMurders, group = 1)) + geom_col() + 
+barMurder <- ggplot(merged_data, aes(x=State, y=TotalMurders, group = 1)) + geom_col() + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
 # Using the above bar chart, sort the x-axis by murder rate
-barMurder <- ggplot(arrestsPop, aes(x=reorder(State,TotalMurders), y=TotalMurders, group = 1)) + geom_col() + 
+barMurder <- ggplot(merged_data, aes(x=reorder(State,TotalMurders), y=TotalMurders, group = 1)) + geom_col() + 
   theme(axis.text.x = element_text(angle = 90, hjust = 1))
 
