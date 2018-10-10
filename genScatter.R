94ca05f5dd011ccac5059fbb77bb384397084a09
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:23:28 -0400

    Saving genScatter.R

--- genScatter.R
+++ genScatter.R
@@ -3,4 +3,4 @@
 # and the Murder rate on size & color
 library(ggplot2)
 scatterMurder <- ggplot(merged_data, aes(x=population, y=percentOver18)) + 
-    geom_point(aes(size=Murder, color=Murder))
+    geom_point(aes(size=Murder, color=Murder)) + ggtitle('Population / Percent Over 18 / Murder Rate Scatter')
