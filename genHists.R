683a958e27f46724e167597ae988ca67a60af5d3
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:26:26 -0400

    Saving genHists.R

--- genHists.R
+++ genHists.R
@@ -1,6 +1,7 @@
 
 # Create a histogram for population
 library(ggplot2)
+# Create the graph and axes
 histPop <- ggplot(merged_data, aes(x=population))
 histPop <- histPop + geom_histogram(binwidth=5000000, color="black", fill="white")
 histPop <- histPop + ggtitle("State Population Histogram")
