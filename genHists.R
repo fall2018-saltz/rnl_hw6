98fe17d1fe6e4fc4b4cbfaf1f4ced33d440430fe
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:27:55 -0400

    Saving genHists.R

--- genHists.R
+++ genHists.R
@@ -10,5 +10,6 @@ histPop <- histPop + ggtitle("State Population Histogram")
 
 # Repeat the same steps to create a histogram for murder rate
 histMurder <- ggplot(merged_data, aes(x=Murder))
+# Adjust the width of the bins to show the data in a way that is valuable to the viewer
 histMurder <- histMurder + geom_histogram(binwidth=1, color="black", fill="white")
 histMurder <- histMurder + ggtitle("State Murder Rate Histogram")
