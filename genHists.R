e66ac67ae4f5002edb54f91bedbd63a3ccc3e465
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-10 15:26:40 -0400

    Saving genHists.R

--- genHists.R
+++ genHists.R
@@ -3,6 +3,7 @@
 library(ggplot2)
 # Create the graph and axes
 histPop <- ggplot(merged_data, aes(x=population))
+# Add the histogram and specify borders and color
 histPop <- histPop + geom_histogram(binwidth=5000000, color="black", fill="white")
 histPop <- histPop + ggtitle("State Population Histogram")
 histMurder <- ggplot(merged_data, aes(x=Murder))
